6781f599668bce5956f8bb6eb60614a0ffe770e8
Koh.RenChien <koh.renchien@projeksistematik.com.my>
2015-08-11 14:00:02 +0800

    commit

--- www/js/homepage.js
+++ www/js/homepage.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -25,6 +26,7 @@ var app = {
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
+    
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
@@ -34,7 +36,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        StatusBar.overlaysWebView(false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
