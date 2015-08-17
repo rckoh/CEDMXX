46fad6695fdee05db635f89ba2fb7a190b8df759
Koh.RenChien <koh.renchien@projeksistematik.com.my>
2015-08-17 12:08:44 +0800

    commit

---
+++ platforms/android/assets/www/js/slidemenu.js
@@ -0,0 +1,48 @@
+$(function(){
+	var menuStatus;
+	
+	$("button.buttonbg").click(function(){
+		if(menuStatus != true){				
+			$(".menuarea").animate({
+                marginLeft: "0px",}, 300, function() {
+                    menuStatus = true; 
+            });
+            
+		  	return false;
+		  } 
+        else {
+			$(".menuarea").animate({
+			marginLeft: "-70%",
+		  }, 300, function(){menuStatus = false;});
+              
+			return false;
+		  }
+	});
+    
+	$("body").on("swipeleft", function(){
+		if (menuStatus){	
+		$(".menuarea").animate({
+			marginLeft: "-70%",
+		  }, 300, function(){menuStatus = false});
+		  }
+	});
+	
+	$("body").on("swiperight", function(){
+		if (!menuStatus){	
+		$(".menuarea").animate({
+			marginLeft: "0%",
+		  }, 300, function(){menuStatus = true});
+		  }
+	});
+	
+	$("#menu li a").click(function(){
+		var p = $(this).parent();
+		if($(p).hasClass('active')){
+			$("#menu li").removeClass('active');
+		} else {
+			$("#menu li").removeClass('active');
+			$(p).addClass('active');
+		}
+	});
+		
+});
---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,26 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.PushPlugin/www/PushNotification.js",
+        "id": "com.phonegap.plugins.PushPlugin.PushNotification",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.hidden-statusbar-overlay/www/hidden-statusbar-overlay.js",
+        "id": "de.appplant.cordova.plugin.hidden-statusbar-overlay.HiddenStatusbarOverlay",
+        "clobbers": [
+            "plugin.statusbarOverlay"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.PushPlugin": "2.5.0",
+    "cordova-plugin-whitelist": "1.0.0",
+    "de.appplant.cordova.plugin.hidden-statusbar-overlay": "1.2.0"
+}
+// BOTTOM OF METADATA
+});
