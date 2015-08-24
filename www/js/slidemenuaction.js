bf2d0d940b42c90a8ad1022327ad66b69d407b30
Koh.RenChien <koh.renchien@projeksistematik.com.my>
2015-08-24 14:47:46 +0800

    commit

---
+++ www/js/slidemenuaction.js
@@ -0,0 +1,9 @@
+function goBusinessMatching(){
+        // $("html").animate({marginLeft: "-100%",}, 300, function() {window.location = "BusinessMatching.html";});
+	window.location = "BusinessMatching.html";
+}
+
+function goTrending(){
+         //$("html").animate({marginLeft: "-100%",}, 300, function() {window.location = "index.html";});
+	window.location = "index.html";
+}
