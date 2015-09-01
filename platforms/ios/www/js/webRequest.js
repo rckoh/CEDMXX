332c4840dd8fb8840c53e2a9c9dcd34dc87c0709
Koh.RenChien <koh.renchien@projeksistematik.com.my>
2015-09-01 11:37:22 +0800

    commit

---
+++ platforms/ios/www/js/webRequest.js
@@ -0,0 +1,33 @@
+var webUrl = "http://netinfinium.publicvm.com:86/";
+
+function getFeaturedList(){
+    alert("webrequst");
+    var requestUrl=webUrl+"drupalgap/getfeatured";
+    
+    $.ajax({
+      url: requestUrl,
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/hal+json"
+      },
+      success: function(data, status, xhr) {
+        debugger
+        var returnstr=JSON.stringify(data);
+          alert(returnstr);
+      },
+      error:function (xhr, ajaxOptions, thrownError){
+        debugger;
+          alert(xhr.status);
+        }
+    })
+}
+
+
+
+                        
+                        
+                        
+                        
+                        
+                        
