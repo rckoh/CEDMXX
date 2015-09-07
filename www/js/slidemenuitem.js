8d1d8936d2c572a6fac736920b063310c4083924
Koh.RenChien <koh.renchien@projeksistematik.com.my>
2015-09-07 12:32:32 +0800

    commit

--- www/js/slidemenuitem.js
+++ www/js/slidemenuitem.js
@@ -2,7 +2,7 @@
 $("#slidemenu").append("<li onclick='goProfile();'><label class='itemlabel'><img src='img/user-2.png' class='menuitemimg' />My Profile</label></li>");
 $("#slidemenu").append("<li onclick='goFavourite();'><label class='itemlabel'><img src='img/fav.png' class='menuitemimg' />Favourites</label></li>");
 $("#slidemenu").append("<li onClick='goTrending();'><label class='itemlabel'><img src='img/trending.png' class='menuitemimg' />Trending</label></li>");
-$("#slidemenu").append("<li onClick='goBusinessMatching();'><label class='itemlabel'><img src='img/Business_Matching.png' class='menuitemimg' />Business Matching</label></li>");
+$("#slidemenu").append("<li onClick='goBusinessMatching();'><label class='itemlabel'><img src='img/business_matching.png' class='menuitemimg' />Business Matching</label></li>");
 $("#slidemenu").append("<li onClick='goListing();'><label class='itemlabel'><img src='img/listing.png' class='menuitemimg' />Listings</label></li>");
 $("#slidemenu").append("<li><label class='itemlabel'><img src='img/lock.png' class='menuitemimg' />Change Password</label></li>");
 $("#slidemenu").append("<li onclick='goAbout();'><label class='itemlabel'><img src='img/about.png' class='menuitemimg' />About eSolutions</label></li>");
