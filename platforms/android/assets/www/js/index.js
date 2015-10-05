b93bfacee929b87fbc87355bec1781c83214248b
Koh.RenChien <koh.renchien@projeksistematik.com.my>
2015-10-05 15:20:53 +0800

    commit

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -19,6 +19,7 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+    
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
@@ -29,6 +30,89 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        
+        var pushNotification = window.plugins.pushNotification;
+        
+        
+        if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
+            pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"16206476952","ecb":"app.onNotificationGCM"});
+        } 
+        else {
+//            pushNotification.registerDevice({alert:true, badge:true, sound:true}, 
+//                function(status) {
+//                app.myLog.value+=JSON.stringify(['registerDevice status: ', status])+"\n";
+//                app.storeToken(status.deviceToken);
+//            });
+            pushNotification.register(
+            app.tokenHandler,
+            app.errorHandler,
+            {
+                "badge":"true",
+                "sound":"true",
+                "alert":"true",
+                "ecb":"app.onNotificationAPN"
+            });
+        }
+
+    },
+    
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Callback Success! Result = '+result)
+    },
+    
+    errorHandler:function(error) {
+        alert(error);
+    },
+    
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+//                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
+    
+    tokenHandler: function(result) {
+        // Your iOS push server needs to know the token before it can push to this device
+        // here is where you might want to send it the token for later use.
+        alert('device token = ' + result);
+    },
+    
+    onNotificationAPN: function(event) {
+        if ( event.alert )
+        {
+            navigator.notification.alert(event.alert);
+        }
+
+        if ( event.sound )
+        {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+
+        if ( event.badge )
+        {
+            pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, event.badge);
+        }
     }
 };
 
